a14e3119d247cc35b3cdee80d5b389a8178d949b
MaelRibes <90307031+MaelRibes@users.noreply.github.com>
2022-05-10 11:45:57 +0200

    TeamController & TeamModel créé

---
+++ app/controller/TeamController.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("app/model/TeamModel.php"); // Import the example Model
+
+class TeamController
+{
+
+    private $teamModel = new TeamModel(); // Bien définir le modèle correspondant
+
+    function displayTeam($teamID)
+    {
+        $team = $this->teamModel->getTeam($teamID);
+        require("app/view/teamView.php");
+    }
+
+    function displayAllTeams($teamID)
+    {
+        $teams = $this->teamModel->getAllTeams();
+        require("app/view/teamView.php");
+    }
+}
+
