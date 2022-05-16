0336bfdbd27bad1383933f3ed8ea4cea81ad9686
ArthurG <arthur.gonay@gmail.com>
2022-05-16 08:56:06 +0200

    first commit

--- app/model/tournamentModel.php
+++ app/model/tournamentModel.php
@@ -14,16 +14,15 @@
 
             $results = mysqli_query($conn, "SELECT * from tournois WHERE id_t =".$tournamentID);
             return $results
-
         }
 
+
+
         
-        function addTournament(){
+        function addTournament($nom,$sport,$capacite,$membres){
             $conn = $this->dbConnect();
 
-
-            // Code pour ajouter un exemple à la bdd
-            // INSERT 
+            $results = mysqli_query($conn,);
 
         }
 
